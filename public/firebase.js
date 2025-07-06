0c467498017d744838a021595f648801bc9f085f
FLS64 <125945735+anoncano@users.noreply.github.com>
2025-07-07 03:07:31 +1000

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,13 +1,24 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyAefBe7cgSA8FNJzR7p5NHWTsbLFrRuyKY",
   authDomain: "the-foundry-by-da.firebaseapp.com",
   projectId: "the-foundry-by-da",
-  storageBucket: "the-foundry-by-da.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  storageBucket: "the-foundry-by-da.firebasestorage.app",
+  messagingSenderId: "825755098252",
+  appId: "1:825755098252:web:ad6e84e40be56271fbefa9",
+  measurementId: "G-72Z53Z6LNR"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize Firebase (unused for now)
 export const app = initializeApp(firebaseConfig);
